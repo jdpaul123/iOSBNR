6afc87cbf38edd09f50f9b2ad392ecdc303270bb
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-04-15 23:23:46 -0700

    now the map view is intantiated through code

--- WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -5,8 +5,27 @@
 //  Created by Jonathan Paul on 4/11/21.
 //
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view.")
