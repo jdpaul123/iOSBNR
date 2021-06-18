eefd57aea77a53d96d14bd5f223f2e172d8b92c4
Jonathan Paul <jpaul@starvista_0895.plsinfo.org>
2021-06-18 14:23:02 -0700

    chapter 6 challenges DONE

---
+++ ch6Challenges/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Jonathan Paul on 4/9/21.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        // Bronze chapter 6 challenge to not allow letters. I also made it not allow symbols
+        var disallowed = CharacterSet.letters
+        disallowed.insert(charactersIn: "!@#$%&*(){}/\"\\';:,><|[]-=_+")
+        var replacementCharSet = CharacterSet()
+        replacementCharSet.insert(charactersIn: string)
+        let badChar = replacementCharSet.isSubset(of: disallowed)
+
+        if existingTextHasDecimalSeperator != nil && replacementTextHasDecimalSeperator != nil || badChar && !string.isEmpty {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+}
---
+++ ch6Challenges/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,109 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jonathan Paul on 4/11/21.
+//
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    // Start Chapter 6 challenge; get users locaiton on map and zoom in
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate: CLLocationCoordinate2D?
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLocationServices()
+        print("MapViewController loaded its view.")
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    private func configureLocationServices() {
+        locationManager.delegate = self
+        
+        let status = locationManager.authorizationStatus
+        
+        if status == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdate(locationManager: locationManager)
+        }
+    }
+    
+    private func beginLocationUpdate(locationManager: CLLocationManager) {
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print("DID GET LATEST LOCATION")
+        
+        guard let latestLocation = locations.first else { return }
+        
+        if currentCoordinate == nil {
+            zoomToLatestLocation(with: latestLocation.coordinate)
+        }
+        
+        currentCoordinate = latestLocation.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("DID CHANGE AUTHORIZATION STATUS")
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdate(locationManager: locationManager)
+        }
+    }
+}
