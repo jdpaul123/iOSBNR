3bd81bd3edbe6915040587cc9df9c725adabd2e3
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-06-27 22:06:49 -0700

    currently about to do page 188 in iOS BNR book

---
+++ LootLogger/LootLogger/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  LootLogger
+//
+//  Created by Jonathan Paul on 6/27/21.
+//
+
+import Foundation
---
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Jonathan Paul on 6/21/21.
+//
+
+import Foundation
