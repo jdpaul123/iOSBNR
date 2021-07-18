c6c8a96b9f1a59b9cf96a98e676690094eb1e184
Jonathan Paul <jpaul@Jonathans-MacBook-Air.local>
2021-07-18 16:13:52 -0700

    Chapter 9 is DONE except not the challenges

--- LootLogger/LootLogger/Item.swift
+++ LootLogger/LootLogger/Item.swift
@@ -5,4 +5,40 @@
 //  Created by Jonathan Paul on 6/27/21.
 //
 
-import Foundation
+import UIKit
+
+class Item: Equatable {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.serialNumber = serialNumber
+        self.valueInDollars = valueInDollars
+        self.dateCreated = Date()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            let randomAdjective = adjectives.randomElement()
+            let randomNoun = nouns.randomElement()
+            
+            let randomName = "\(String(describing: randomAdjective)) \(String(describing: randomNoun))"
+            let randomValue = Int.random(in: 0 ..< 100)
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+    
+    static func ==(lhs: Item, rhs: Item) -> Bool {
+        return lhs.name == rhs.name && lhs.serialNumber == rhs.serialNumber && lhs.dateCreated == rhs.dateCreated
+    }
+}
